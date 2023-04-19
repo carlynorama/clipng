906fed4e36fdd05b874153187438a90611a704b7
carlynorama <carlynorama@gmail.com>
2023-04-19 16:17:03 -0700

    isolate URL, add example funcs

--- Sources/clipng/FileIO.swift
+++ Sources/clipng/FileIO.swift
@@ -28,22 +28,74 @@ struct FileIO {
         }
     }
     
-    static func writeDataToFile(data:Data , filePath:String) throws {
-        //TODO: For iOS?? 
+    static func makeFileURL(filePath:String) -> URL {
+        //TODO: For iOS??
         //let locationToWrite = URL.documentsDirectory.appendingPathComponent("testImage", conformingTo: .png)
-        
         #if os(Linux)
-        try data.write(to:URL(fileURLWithPath: filePath))
+        return URL(fileURLWithPath: filePath)
         #else
         if #available(macOS 13.0, *) {
-            try data.write(to: URL(filePath: filePath))
+            return URL(filePath: filePath)
         } else {
             // Fallback on earlier versions
-            try data.write(to:URL(fileURLWithPath: filePath))
+             return URL(fileURLWithPath: filePath)
         }
         #endif
-        
-        
-        
     }
+    
+    
+    static func writeDataToFile(data:Data , filePath:String) throws {
+        let url = makeFileURL(filePath: filePath)
+        try data.write(to: url)
+    }
+    
+    //---------
+    //https://www.swiftbysundell.com/articles/working-with-files-and-folders-in-swift/
+    
+    //FileManager.default.temporaryDirectory, .cachesDirectory
+    static func write<T: Encodable>(
+        _ value: T,
+        toDocumentNamed documentName: String,
+        encodedUsing encoder: JSONEncoder = .init()
+    ) throws {
+        let folderURL = try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
+
+        let fileURL = folderURL.appendingPathComponent(documentName)
+        let data = try encoder.encode(value)
+        try data.write(to: fileURL)
+    }
+    
+    //TODO: What happens when folders in a path don't exist with writeDataToFile
+    @available(macOS 13.0, *)
+    static func write<T: Encodable>(
+        _ value: T,
+        toDocumentNamed documentName: String,
+        inFolder folderName: String,
+        encodedUsing encoder: JSONEncoder = .init()
+    ) throws {
+        let folderURL = try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        ).appending(component: folderName)
+        
+        if !FileManager.default.fileExists(atPath: folderURL.relativePath) {
+            try FileManager.default.createDirectory(
+                at: folderURL,
+                withIntermediateDirectories: false,
+                attributes: nil
+            )
+        }
+
+        let fileURL = folderURL.appendingPathComponent(documentName)
+        let data = try encoder.encode(value)
+        try data.write(to: fileURL)
+    }
+    
 }
