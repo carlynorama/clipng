c51b01041e575e6d4c522982e0b90562cf7037e5
carlynorama <carlynorama@gmail.com>
2023-05-01 10:31:24 -0700

    point package at main

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "clipng",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
-        .package(url: "https://github.com/carlynorama/SwiftLIBPNG.git", branch:"dev-for-CLI")
+        .package(url: "https://github.com/carlynorama/SwiftLIBPNG.git", from:"0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
